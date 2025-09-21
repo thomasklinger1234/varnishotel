ef3383b035053dd7a43c895179fade9ea5dc87fe
Thomas Klinger <thomas.klinger@otto.de>
2025-09-21 23:10:03 +0200

    chore: add tracing and logs
    The tracing crate is added to supporting logging. By default,
    the log level is set to a moderate value (info) for the crate
    but can be configured by the user via the `RUST_LOG` environment
    variable.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use tracing_subscriber::prelude::*;
 
 #[derive(Debug, Parser, Clone)]
 #[command(name = "varnishotel")]
@@ -8,6 +9,19 @@ pub struct App {}
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let _ = App::parse();
+
+    let app_name = env!("CARGO_CRATE_NAME");
+    let app_version = env!("CARGO_PKG_VERSION");
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=info", app_name).into()),
+        )
+        .init();
+
+    tracing::info!(app_name, app_version);
 
     Ok(())
 }
